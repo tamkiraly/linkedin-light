edd887c7a86d36c8daf84d1b8118d6a241ecb215
Tamas Kiraly <tamaskiraly90@gmail.com>
2022-09-14 18:09:16 +0200

    Initialize Spring project

---
+++ src/main/java/com/tamkiraly/linkedinlight/LinkedinLightApplication.java
@@ -0,0 +1,13 @@
+package com.tamkiraly.linkedinlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinLightApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LinkedinLightApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tamkiraly/linkedinlight/LinkedinLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamkiraly.linkedinlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinLightApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
