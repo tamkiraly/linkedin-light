a99b301a1119740c6f743920c5b40532ea87b83b
Tamas Kiraly <tamaskiraly90@gmail.com>
2022-09-15 22:51:07 +0200

    Add Flyway dependency, setup migrations, update Client and Position models

--- src/main/java/com/tamkiraly/linkedinlight/models/Client.java
+++ src/main/java/com/tamkiraly/linkedinlight/models/Client.java
@@ -5,23 +5,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 
 @Entity
+@Table(name = "client")
 @Getter
 public class Client {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String apiKey;
-  private String name;
+  private String clientName;
   private String email;
 
   public Client() {
   }
 
   public Client(String name, String email) {
-    this.name = name;
+    this.clientName = name;
     this.email = email;
     this.apiKey = UUID.randomUUID().toString();
   }
