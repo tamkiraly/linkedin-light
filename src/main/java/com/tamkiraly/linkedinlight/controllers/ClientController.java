8373470f316b01227548ecd0792093240804085d
Tamas Kiraly <tamaskiraly90@gmail.com>
2022-09-16 18:19:27 +0200

    Add apiInfo to SwaggerConfig, add ApiOperation annotation to /client endpoint

--- src/main/java/com/tamkiraly/linkedinlight/controllers/ClientController.java
+++ src/main/java/com/tamkiraly/linkedinlight/controllers/ClientController.java
@@ -1,24 +1,27 @@
 package com.tamkiraly.linkedinlight.controllers;
 
 import com.tamkiraly.linkedinlight.dtos.ClientRegistrationRequestDTO;
+import com.tamkiraly.linkedinlight.dtos.ClientRegistrationResponseDTO;
 import com.tamkiraly.linkedinlight.models.Client;
 import com.tamkiraly.linkedinlight.services.ClientService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/client")
 @AllArgsConstructor
 public class ClientController {
 
   private final ClientService clientService;
 
-  @PostMapping
+  @PostMapping("/client")
+  @ApiOperation(value = "Registers a new client in the database, returns an API key",
+      notes = "Provide name and email in JSON format",
+      response = ClientRegistrationResponseDTO.class)
   public ResponseEntity<?> registerNewClient(@RequestBody ClientRegistrationRequestDTO requestDTO) {
     Client newClient = clientService.handleRequestDTO(requestDTO);
     return ResponseEntity.status(HttpStatus.CREATED).body(clientService.extractAPIKeyFromClient(newClient));
--- src/main/java/com/tamkiraly/linkedinlight/controllers/PositionController.java
+++ src/main/java/com/tamkiraly/linkedinlight/controllers/PositionController.java
@@ -11,11 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/position")
 public class PositionController {
 
   private final PositionService positionService;
@@ -25,14 +23,14 @@ public class PositionController {
     this.positionService = positionService;
   }
 
-  @PostMapping("/")
+  @PostMapping("/position")
   public ResponseEntity<?> createNewPosition(@RequestBody PositionCreationRequestDTO requestDTO) {
     Position newPosition = positionService.handlePositionRequestDTO(requestDTO);
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(new PositionCreationResponseDTO(newPosition.getPositionUrl()));
   }
 
-  @GetMapping("/search")
+  @GetMapping("/position/search")
   public ResponseEntity<?> returnPositionListDTO(@RequestBody PositionSearchDTO searchDTO) {
     positionService.validateSearchDTO(searchDTO);
     return ResponseEntity.status(HttpStatus.OK).body(positionService.createPositionUrlDTOList(searchDTO));
