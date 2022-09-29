deaa437536c563dd2a0ff3fbad0e387511cd39c7
Tamas Kiraly <tamaskiraly90@gmail.com>
2022-09-29 21:12:12 +0200

    Add test cases to PositionServiceTest

--- src/test/java/com/tamkiraly/linkedinlight/TestConfiguration.java
+++ src/test/java/com/tamkiraly/linkedinlight/TestConfiguration.java
@@ -1,6 +1,8 @@
 package com.tamkiraly.linkedinlight;
 
 import com.tamkiraly.linkedinlight.dtos.PositionCreationRequestDTO;
+import com.tamkiraly.linkedinlight.dtos.PositionSearchDTO;
+import com.tamkiraly.linkedinlight.models.Position;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -18,4 +20,24 @@ public class TestConfiguration {
     return requestDTO;
   }
 
+  @Bean(name = "searchRequest")
+  @Scope(value = "prototype")
+  public PositionSearchDTO getSearchDTO() {
+    PositionSearchDTO searchDTO = new PositionSearchDTO();
+    searchDTO.setApiKey("0218fa78-7404-46f2-ae2e-6fc78b93247b");
+    searchDTO.setKeyword("developer");
+    searchDTO.setLocation("Budapest");
+    return searchDTO;
+  }
+
+  @Bean(name = "position10")
+  @Scope(value = "prototype")
+  public Position getPosition() {
+    Position position = new Position();
+    position.setId(10L);
+    position.setPositionUrl();
+    position.setPositionName("Developer apprentice");
+    position.setPositionLocation("Budapest");
+    return position;
+  }
 }
--- src/test/java/com/tamkiraly/linkedinlight/unittests/PositionServiceTest.java
+++ src/test/java/com/tamkiraly/linkedinlight/unittests/PositionServiceTest.java
@@ -1,14 +1,22 @@
 package com.tamkiraly.linkedinlight.unittests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 import com.tamkiraly.linkedinlight.TestConfiguration;
 import com.tamkiraly.linkedinlight.dtos.PositionCreationRequestDTO;
+import com.tamkiraly.linkedinlight.dtos.PositionSearchDTO;
+import com.tamkiraly.linkedinlight.dtos.PositionUrlDTO;
+import com.tamkiraly.linkedinlight.exceptions.InvalidApiKeyException;
+import com.tamkiraly.linkedinlight.exceptions.InvalidLocationNameException;
 import com.tamkiraly.linkedinlight.exceptions.InvalidParameterException;
+import com.tamkiraly.linkedinlight.models.Position;
 import com.tamkiraly.linkedinlight.repositories.ClientRepository;
 import com.tamkiraly.linkedinlight.repositories.PositionRepository;
 import com.tamkiraly.linkedinlight.services.PositionService;
 import com.tamkiraly.linkedinlight.services.PositionServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -33,9 +41,7 @@ public class PositionServiceTest {
 
   private final ClientRepository clientRepository = Mockito.mock(ClientRepository.class);
   private final PositionRepository positionRepository = Mockito.mock(PositionRepository.class);
-
   private final PositionService positionService = new PositionServiceImpl(positionRepository, clientRepository);
-
 
   @Test
   public void validatePositionRequestDto_WhenNameIsBlank_ThrowInvalidParameterException() {
@@ -45,8 +51,68 @@ public class PositionServiceTest {
     InvalidParameterException exception = Assertions.assertThrows(InvalidParameterException.class, () -> {
       Mockito.when(clientRepository.existsByApiKey(requestDTO.getApiKey())).thenReturn(true);
       positionService.validatePositionRequestDTO(requestDTO);
-    }, "InvalidParameterException was excpected");
+    }, "InvalidParameterException was expected");
 
     assertEquals("Please provide a name for the position.", exception.getMessage());
+  }
+
+  @Test
+  public void validatePositionRequestDto_WhenNoClientFoundByApiKey_throwInvalidApiKeyException() {
+    PositionCreationRequestDTO requestDTO = beanFactory.getBean("noNameRequest", PositionCreationRequestDTO.class);
+    requestDTO.setApiKey("1234-5678-9011-1213");
+
+    InvalidApiKeyException exception = Assertions.assertThrows(InvalidApiKeyException.class, () -> {
+      Mockito.when(clientRepository.existsByApiKey(requestDTO.getApiKey())).thenReturn(false);
+      positionService.validatePositionRequestDTO(requestDTO);
+    }, "InvalidApiKeyException was expected");
+
+    assertEquals("API key is invalid.", exception.getMessage());
+  }
+
+  @Test
+  public void validatePositionRequestDto_WhenLocationNameIsTooLong_ThrowInvalidLocationNameException() {
+    PositionCreationRequestDTO requestDTO = beanFactory.getBean("noNameRequest", PositionCreationRequestDTO.class);
+    // although that's actually a valid location:
+    // https://www.ricksteves.com/watch-read-listen/read/articles/small-town-with-big-name
+    requestDTO.setPositionLocation("Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch");
+
+    InvalidLocationNameException exception = Assertions.assertThrows(InvalidLocationNameException.class, () -> {
+      Mockito.when(clientRepository.existsByApiKey(requestDTO.getApiKey())).thenReturn(true);
+      positionService.validatePositionRequestDTO(requestDTO);
+    }, "InvalidLocationNameException was expected");
+
+    assertEquals("Location's name should not be longer than 50 characters", exception.getMessage());
+  }
+
+  @Test
+  public void generateUrlForNewPosition_ShouldReturn_NewPosition() {
+    PositionCreationRequestDTO requestDTO = beanFactory.getBean("noNameRequest", PositionCreationRequestDTO.class);
+    Position position = new Position(requestDTO.getPositionName(), requestDTO.getPositionLocation());
+
+    Mockito.when(positionRepository.save(any(Position.class))).thenReturn(position);
+    positionService.generateUrlForNewPosition(requestDTO);
+
+    Mockito.verify(positionRepository, Mockito.times(1)).save(position);
+  }
+
+  @Test
+  public void createPositionUrlDtoList_GivenMatchingParameters_ShouldReturnDtoList() {
+    Position position = beanFactory.getBean("position10", Position.class);
+    PositionSearchDTO searchDTO = beanFactory.getBean("searchRequest", PositionSearchDTO.class);
+    List<Position> positionList = new ArrayList<>();
+    positionList.add(position);
+
+    Mockito.when(positionRepository.findAllByNameAndLocation(searchDTO.getKeyword(), searchDTO.getLocation()))
+        .thenReturn(positionList);
+    positionService.createPositionUrlDTOList(searchDTO);
+
+    Mockito.verify(positionRepository, Mockito.times(1))
+        .findAllByNameAndLocation(searchDTO.getKeyword(), searchDTO.getLocation());
+
+    List<PositionUrlDTO> urlDTOList = new ArrayList<>();
+    urlDTOList.add(new PositionUrlDTO(position.getPositionUrl()));
+
+    assertEquals(urlDTOList.get(0).getPositionUrl(),
+        positionService.createPositionUrlDTOList(searchDTO).get(0).getPositionUrl());
   }
 }
